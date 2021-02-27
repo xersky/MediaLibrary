6b5c23897e953608c80afa88d9a833fa0754b637
xersky <59363324+xersky@users.noreply.github.com>
2021-02-27 22:59:09 +0100

    >Application page Menus

--- src/ApplicationPage.java
+++ src/ApplicationPage.java
@@ -1,6 +1,55 @@
 public class ApplicationPage {
-    public static void Menu(){
-
+    public static void menuApp(){
+        System.out.println("------------------Application Page Menu------------------");
+        System.out.println("#      1- Show All Documents");
+        System.out.println("#      2- Show All Books");
+        System.out.println("#      3- Show All Images");
+        System.out.println("#      4- Show All Videos");
+        System.out.println("#      5- Show All Audios");
+        System.out.println("#      6- Search for a document");
+        System.out.println("#      7- Display a document");
+        System.out.println("#      8- Modify a document");
+        System.out.println("#      9- Delete a document");
+        System.out.println("#      10- Add a new document");
+        System.out.println("#      5- Go back");
+        System.out.println("---------------------------------------------------------");
     }
+    public static void menuSearchDoc(){
+        System.out.println("------------------Search for a document Menu------------------");
+        System.out.println("#      1- Search for a book");
+        System.out.println("#      2- Search for an image");
+        System.out.println("#      3- Search for an audio");
+        System.out.println("#      4- Search for a video");
+        System.out.println("#      5- Go back");
+        System.out.println("--------------------------------------------------------------");
+    }
+    public static void menuDisplayDoc(){
+        System.out.println("------------------Display a document Menu------------------");
+        System.out.println("#      1- Display for a book");
+        System.out.println("#      2- Display for an image");
+        System.out.println("#      3- Display for an audio");
+        System.out.println("#      4- Display for a video");
+        System.out.println("#      5- Go back");
+        System.out.println("-----------------------------------------------------------");
+    }
+    public static void menuDeleteDoc(){
+        System.out.println("------------------Delete a document Menu------------------");
+        System.out.println("#      1- Delete for a book");
+        System.out.println("#      2- Delete for an image");
+        System.out.println("#      3- Delete for an audio");
+        System.out.println("#      4- Delete for a video");
+        System.out.println("#      5- Go back");
+        System.out.println("-----------------------------------------------------------");
+    }
+    public static void menuAddDoc(){
+        System.out.println("------------------Add a document Menu------------------");
+        System.out.println("#      1- Add for a book");
+        System.out.println("#      2- Add for an image");
+        System.out.println("#      3- Add for an audio");
+        System.out.println("#      4- Add for a video");
+        System.out.println("#      5- Go back");
+        System.out.println("-------------------------------------------------------");
+    }
+
 }
 
